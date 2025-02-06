c829ded170ce027511a56e66060fd6002d7016eb
Erumsikander <mrsikanderghouri@gmail.com>
2025-02-06 05:28:01 +0500

    one

--- template-0/src/app/components/Navbar.tsx
+++ template-0/src/app/components/Navbar.tsx
@@ -10,11 +10,11 @@ export default function Navbar() {
       {/* Navbar Main Container */}
       <div className="p-4 flex items-center justify-between max-w-5xl mx-auto flex-nowrap">
         {/* Logo  */}
-        <div className="flex items-center mx-auto">
-          <FaShopify className="w-10 h-10 rounded-full hover:text-amber-400 text-lime-400 cursor-pointer" />
+        <div className="flex  items-center mx-auto">
+          <FaShopify className="w-10 h-10 mr-auto rounded-full hover:text-amber-400 text-lime-400 cursor-pointer" />
           <div className="relative hidden lg:block md:block">
             <input
-              className="rounded-3xl py-3 px-3 outline-none text-xs w-[350px] pr-10 text-black"
+              className="rounded-3xl py-3 px-3 outline-none text-xs w-[300px] pr-10 text-black"
               placeholder="Explore our best sellers and new arrivals...!"
             />
             <FaSearch className="w-5 h-5 text-slate-700 absolute right-3 top-1/2 transform -translate-y-1/2" />
